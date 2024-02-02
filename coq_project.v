4dea05e72ad89c5df420469ff7134962979356cc
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2024-02-02 18:46:50 +0330

    implementing

--- coq_project.v
+++ coq_project.v
@@ -23,7 +23,7 @@ Fixpoint evaluate_with_env (expr: Expression) (env: list (string * nat)) : EvalR
   | Num n => Ok n
   | Var v => match lookup_variable v env with
              | Some n => Ok n
-             | None => Error
+             | _ => Error
              end
   | Plus l r => match (evaluate_with_env l env, evaluate_with_env r env) with
                 | (Ok n1, Ok n2) => Ok (n1 + n2)
@@ -32,7 +32,7 @@ Fixpoint evaluate_with_env (expr: Expression) (env: list (string * nat)) : EvalR
   | Let var initializer result =>
     match evaluate_with_env initializer env with
     | Ok n => evaluate_with_env result ((var, n) :: env)
-    | Error => Error
+    | _ => Error
     end
   end.
 
@@ -70,16 +70,21 @@ Lemma evaluate_plus_comm: forall e1 e2, evaluate (Plus e1 e2) =
 evaluate (Plus e2 e1).
 Proof.
 intros e1 e2.
-unfold evaluate in *.
+unfold evaluate.
 simpl.
 destruct evaluate_with_env.
 destruct evaluate_with_env.
--rewrite Nat.add_comm. reflexivity.
--reflexivity.
--destruct evaluate_with_env.
+- rewrite Nat.add_comm. reflexivity.
+- reflexivity.
+- destruct evaluate_with_env.
   +reflexivity.
   +reflexivity.
 Qed.
+
+Lemma extended_env_error_implies_env_error: forall e2 var n,
+evaluate_with_env e2 ((var, n) :: nil) = Error -> evaluate e2 = Error.
+Proof.
+Admitted.
 
 Lemma error_implies_free: forall e,
  evaluate e = Error -> exists v : string, IsFree v e.
@@ -96,17 +101,36 @@ intro e. intro H. induction e.
     -- assert (exists v : string, IsFree v e1). apply IHe1. reflexivity. 
           destruct H0 as [v1 F]. exists v1. apply IsFreePlusLeft. exact F.
   + destruct (evaluate e1) eqn:E1.
-    -- unfold evaluate in H. simpl in H. destruct (evaluate_with_env e1 nil) eqn:Eq1.
-      destruct (evaluate_with_env e2 ((var, n0) :: nil)) eqn:Eq2. 
-        ++ discriminate.
-        ++ assert (exists v : string, IsFree v e2). apply IHe2. admit.
-          *destruct H0 as [v1 H0']. exists v1. apply IsFreeLetBody. exact H0'.
-          clear e1 Eq1 E1 IHe1 H n. 
-unfold not.
-intros H. rewrite H in H0'. unfold evaluate_with_env in Eq2. admit.
-        ++ unfold evaluate in E1. rewrite Eq1 in E1. discriminate.
+    -- clear IHe1. unfold evaluate in H. simpl in H. unfold evaluate in E1. rewrite E1 in H.
+      clear E1. destruct (evaluate e2) eqn:Eq2.
+      ++ clear IHe2. pose proof (extended_env_error_implies_env_error e2 var n).
+          assert (evaluate e2 = Error). rewrite H0. reflexivity. exact H.
+           rewrite H1 in Eq2. discriminate.
+      ++ assert (exists v : string, IsFree v e2). apply IHe2. reflexivity. clear IHe2.
+         destruct H0 as[v1 F]. exists v1. apply IsFreeLetBody. exact F.
+          unfold not. intros HF. rewrite HF in F. clear HF v1. 
+admit.
     -- assert (exists v : string, IsFree v e1). apply IHe1. reflexivity.
-        ++ destruct H0 as [v1 H0']. exists v1. apply IsFreeLetInit. exact H0'.
+        destruct H0 as [v1 H0']. exists v1. apply IsFreeLetInit. exact H0'.
+Admitted.
+
+Lemma Free_implies_error : forall e,
+(exists v : string, IsFree v e) -> evaluate e = Error.
+Proof.
+intros e.
+intro H.  induction e.
+- destruct H as [v F]. inversion F.
+- destruct H as [v0 F].
+destruct (lookup_variable v nil) as [n |] eqn:L. discriminate.
+unfold evaluate. unfold evaluate_with_env. rewrite L. reflexivity.
+- destruct H as [v F]. unfold evaluate. simpl. destruct (evaluate e1) eqn:E1.
+  + unfold evaluate in E1. rewrite E1. assert (evaluate e2 = Error).
+    -- rewrite IHe2. reflexivity. clear IHe1. admit.
+    -- unfold evaluate in H. rewrite H. reflexivity.
+  + unfold evaluate in E1. rewrite E1. reflexivity.
+- destruct H as [v F]. unfold evaluate. simpl. destruct (evaluate e1) eqn:E1.
+  + unfold evaluate in E1. rewrite E1. admit.
+  + unfold evaluate in E1. rewrite E1. reflexivity.
 Admitted.
 
 Lemma evaluate_error: forall e, evaluate e = Error <-> exists v,
@@ -114,16 +138,6 @@ IsFree v e.
 Proof.
 intros e. split.
 - apply error_implies_free.
-- intro H. induction e.
-  + destruct H as [v F]. inversion F.
-  + destruct H as [v0 F]. inversion F. 
-  destruct (lookup_variable v nil) as [n |] eqn:L. discriminate.
-  unfold evaluate. unfold evaluate_with_env. rewrite L. reflexivity.
-  + destruct H as [v F]. unfold evaluate. simpl. destruct (evaluate e1) eqn:E1.
-    -- unfold evaluate in E1. rewrite E1. admit.
-    -- unfold evaluate in E1. rewrite E1. reflexivity.
-  + destruct H as [v F]. unfold evaluate. simpl. destruct (evaluate e1) eqn:E1.
-    -- unfold evaluate in E1. rewrite E1. admit.
-    --  unfold evaluate in E1. rewrite E1. reflexivity.
-Admitted.
+- apply Free_implies_error.
+Qed.
 
